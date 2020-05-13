1b4c233f7931f5e5211033db3395708b8d597401
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-13 15:33:05 +0530

    Initialising packages in app.js

--- app.js
+++ app.js
@@ -4,5 +4,18 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 
+//Initialising express, body-parser and ejs
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+//Using public directory
+app.use(express.static("public"));
+
 //Initialising mongoose
 mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+//Creating instance for app
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
